8f53d7fd585391043218cbea3fade5ea1af5ec02
LOCO185 <jloco151@gmail.com>
2022-06-15 22:27:20 +0300

    imported Results into Routes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import Results from "./Results";
 
 const Routes = () => {
   return (
     <div>
-        <h1>Routes</h1>
+      <h1>Routes</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Routes
+export default Routes;
