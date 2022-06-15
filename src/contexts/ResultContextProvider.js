34c63e51b9d9c12d7817e29e6aaf74a6787bc475
LOCO185 <jloco151@gmail.com>
2022-06-15 22:52:09 +0300

    Added ResultContextProvider.js

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,15 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const StateContextProvider = ({ children }) => {
+
+  return (
+    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
