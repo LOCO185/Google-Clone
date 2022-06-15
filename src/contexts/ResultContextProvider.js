f33f090b1461c8707541506615b20d037024f60e
LOCO185 <jloco151@gmail.com>
2022-06-15 22:57:11 +0300

    Added getResults Function

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -8,6 +8,23 @@ export const StateContextProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
 
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "google-search3.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
   return (
     <StateContext.Provider
       value={{ getResults, results, searchTerm, setSearchTerm, loading }}
