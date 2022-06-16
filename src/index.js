7707ddbeccbc2a15b030427f9a27ccba20e8ed37
LOCO185 <jloco151@gmail.com>
2022-06-16 13:03:35 +0300

    Added StateContextProvider into index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import { StateContextProvider } from "./contexts/ResultContextProvider";
 import "./global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <StateContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </StateContextProvider>
   </React.StrictMode>
 );
