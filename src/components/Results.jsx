4c3aa3e36c8cc863fae283b02f7707b1213971e9
LOCO185 <jloco151@gmail.com>
2022-06-16 13:07:24 +0300

    Added Results component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactPlayer from "react-player";
+
+import { useStateContext } from "../contexts/ResultContextProvider";
+import { Loading } from "./Loading";
 
 const Results = () => {
-  return <div>Results</div>;
+  const { results, isLoading, getResults, searchTerm } = useStateContext();
+  const location = useLocation();
+
+
+  if (isLoading) return <Loading />;
+
+  switch (location.pathname) {
+    case "/search":
+      return "SEARCH";
+    case "/images":
+      return "SEARCH";
+
+    case "/news":
+      return "SEARCH";
+
+    case "/videos":
+      return "SEARCH";
+
+    default:
+      return "ERROR";
+  }
 };
 
 export default Results;
