3e76d2d4251efd773a81bff8cf06b6e06e8dedff
LOCO185 <jloco151@gmail.com>
2022-06-16 18:06:57 +0300

    ..

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -16,7 +16,7 @@ export const StateContextProvider = ({ children }) => {
       headers: {
         "x-user-agent":'desktop',
         "x-rapidapi-host": "google-search3.p.rapidapi.com",
-        "x-rapidapi-key": 'd7a27e4e7dmsh2e39f755ee47d71p1b9f1bjsnd6a4189ba591',
+        // "x-rapidapi-key": 'd7a27e4e7dmsh2e39f755ee47d71p1b9f1bjsnd6a4189ba591',
         // "x-rapidapi-key": process.env.REACT_APP_API_KEY,
       },
     });
