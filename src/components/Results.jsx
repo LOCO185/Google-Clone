6f05171102fef34a825301abb2d417e9dbed01e6
LOCO185 <jloco151@gmail.com>
2022-06-16 13:10:43 +0300

    Added search result example in Results.jsx

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -17,7 +17,23 @@ const Results = () => {
 
   switch (location.pathname) {
     case "/search":
-      return "SEARCH";
+      return (
+        <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+          {results?.results?.map(({ link, title }, index) => (
+            <div key={index} className="md:w-2/5 w-full">
+              <a href={link} target="_blank" rel="noreferrer">
+                <p className="text-sm">
+                  {link.length > 30 ? link.substring(0, 30) : link}
+                </p>
+                <p className="text-lg hover:underline dark:text-blue-30 text-blue-700">
+                  {title}
+                </p>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
+
     case "/images":
       return "SEARCH";
 
@@ -30,6 +46,11 @@ const Results = () => {
     default:
       return "ERROR";
   }
+  // return (
+  // <div>
+  //   Results
+  //   </div>
+  // )
 };
 
 export default Results;
