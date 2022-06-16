b4e3bdf833bac9edcf6aa4d57724ab26cdf97041
LOCO185 <jloco151@gmail.com>
2022-06-16 13:04:53 +0300

    Fixes and changes in ResultContextProvider.js

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -8,18 +8,21 @@ export const StateContextProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
 
-  const getResults = async (url) => {
+  const getResults = async (type) => {
     setLoading(true);
 
-    const res = await fetch(`${baseUrl}${url}`, {
+    const res = await fetch(`${baseUrl}${type}`, {
       method: "GET",
       headers: {
+        "x-user-agent":'desktop',
         "x-rapidapi-host": "google-search3.p.rapidapi.com",
-        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+        "x-rapidapi-key": 'd7a27e4e7dmsh2e39f755ee47d71p1b9f1bjsnd6a4189ba591',
+        // "x-rapidapi-key": process.env.REACT_APP_API_KEY,
       },
     });
 
     const data = await res.json();
+    console.log(data);
 
     setResults(data);
     setLoading(false);
