3f647c62094683003c3dfef0480c5665dd41465a
LOCO185 <jloco151@gmail.com>
2022-06-21 23:18:24 +0300

    Refactor Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
 
-const Search = () => {
+import { useStateContext } from "../contexts/StateContextProvider";
+import { Links } from "./Links";
+
+export const Search = () => {
+  const { setSearchTerm } = useStateContext();
+  const [text, setText] = useState("Elon Musk");
+  const [debouncedValue] = useDebounce(text, 300);
+
+  useEffect(() => {
+    if (debouncedValue) setSearchTerm(debouncedValue);
+  }, [debouncedValue]);
+
   return (
-    <div>
-      <h1>Search</h1>
+    <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+        Links
     </div>
   );
 };
-
-export default Search;
