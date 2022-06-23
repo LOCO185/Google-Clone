856d29a74d89f21b0b915da26596ed5d965e08e9
LOCO185 <jloco151@gmail.com>
2022-06-23 16:58:31 +0300

    final refactor

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ReactPlayer from "react-player";
 
-import { useStateContext } from "../contexts/StateContextProvider";
-import { Loading } from "./Loading";
+import { useStateContext } from "../contexts/ResultContextProvider";
+import Loading from "./Loading";
 
 export const Results = () => {
   const { results, loading, getResults, searchTerm } = useStateContext();
